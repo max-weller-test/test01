7623cac3948c2325c43e8ae100da3e73bdbb4fea
auto-committer <auto-committer@example.com>
2018-01-15 17:18:20 +0100

    create/update simulationTemplate

--- install.py
+++ install.py
@@ -1,9 +1,12 @@
 import framework
 from time import sleep
 
+# test 2
+
 if __name__ == '__main__':
 	framework.start()
 	framework.record("a", framework.param('a'))
 	sleep(2)
 	framework.record("b", framework.param('b'))
 	framework.stop()
+
